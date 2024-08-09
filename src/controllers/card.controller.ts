d97ab8973dab4fe87850e6685fd2461c10474d0d
Juanse1108 <juansetonog@gmail.com>
2024-08-08 21:22:52 -0500

    se ajusta ipAddres

--- src/controllers/card.controller.ts
+++ src/controllers/card.controller.ts
@@ -9,18 +9,23 @@ export const createMemberAndMembership = async (req: Request, res: Response) => 
   try {
     const { number, code, memberData } = req.body;
     const registrationDate = moment().tz('America/Bogota').toDate();
-    const ipAddress = req.ip;
+    
+    // Capturar la IP del cliente y truncarla a 15 caracteres si es necesario
+    const ipAddress = req.ip.substring(0, 15);
 
+    // Validar y activar la tarjeta
     const card = await CardRepository.validateAndActivateCard(number, code, registrationDate);
     if (!card) {
       return res.status(404).json({ message: 'Card not found' });
     }
 
+    // Verificar si el email ya existe
     const existingMember = await CardRepository.findMemberByEmail(memberData.email);
     if (existingMember) {
       return res.status(400).json({ message: 'The email address is already registered.' });
     }
 
+    // Crear miembro
     const memberIdNum = await CardRepository.createMember({
       ...memberData,
       agreement_id: memberData.agreement_id,
@@ -36,6 +41,7 @@ export const createMemberAndMembership = async (req: Request, res: Response) => 
     const publicId = uuidv4();
     const publicIdBinary = Buffer.from(publicId.replace(/-/g, ''), 'hex');
 
+    // Crear membresía
     const membership = await CardRepository.createMembership(
       memberIdNum, 
       card.id,
