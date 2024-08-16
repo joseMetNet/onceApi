02d09515f5c71d15cd3881215e07f0b7893df8f0
Juanse1108 <juansetonog@gmail.com>
2024-08-16 16:45:20 -0500

    se hacen validaciones y encriptacion de password

--- src/repository/card.repository.ts
+++ src/repository/card.repository.ts
@@ -3,20 +3,28 @@ import Member from '../models/member.model';
 import Membership from '../models/membership.model';
 import Agreement from '../models/agreement.model';
 import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
 class CardRepository {
+  // Método para validar y activar la tarjeta
   async validateAndActivateCard(number: string, code: string, registrationDate: Date): Promise<Card | null> {
     const card: any = await Card.findOne({ where: { number, code } });
 
-    if (card) {
-      if (registrationDate > card.expires_at) {
-        throw new Error('Card has expired');
-      }
-      card.status = 'active';
-      await card.save();
-      return card;
+    if (!card) {
+      throw new Error('Card not found'); 
     }
-    return null;
+
+    if (card.status === 'active') {
+      throw new Error('Card is already active');
+    }
+
+    if (registrationDate > card.expires_at) {
+      throw new Error('Card has expired');
+    }
+
+    card.status = 'active';
+    await card.save();
+    return card;
   }
 
   async getAgreementIdById(id: number): Promise<string | null> {
@@ -36,26 +44,29 @@ class CardRepository {
     if (!agreementId) {
       throw new Error('Agreement ID does not exist');
     }
-  
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(memberData.password, saltRounds);
+
     const memberId = uuidv4();
     const memberIdBinary = Buffer.from(memberId.replace(/-/g, ''), 'hex');
-  
+
+    memberData.password = hashedPassword;
     memberData.agreement_id = Buffer.from(agreementId.replace(/-/g, ''), 'hex');
     memberData.member_id = memberIdBinary;
-
     memberData.preferred_department_id = Buffer.from(memberData.preferred_department_id.replace(/-/g, ''), 'hex');
-  
+
     const member = await Member.create(memberData);
     return member.id;
   }
-  
+
   async createMembership(memberId: number, cardId: number, publicId: Buffer): Promise<Membership> {
     const membershipData = {
       public_id: publicId,
       member_id: memberId,
       card_id: cardId,
     };
-  
+
     const membership = await Membership.create(membershipData);
     return membership;
   }
